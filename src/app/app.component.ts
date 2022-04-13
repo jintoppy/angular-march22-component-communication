f0d726658fa108bd382321e78789c566779d875a
Jinto Jose <jintoppy@gmail.com>
2022-04-13 15:32:24 +0200

    initial

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './models/user';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'component-input-output-communication';
+  users: User[] = []
 }
